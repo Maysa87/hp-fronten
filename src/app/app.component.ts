bd5a079c324ce7927d75da5315698a96e8b3dc54
Maysa Leocadio <lmaysagomes@gmail.com>
2025-06-20 13:02:02 -0300

    feat: adding frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HpService } from './services/hp.service';
+import { Component, importProvidersFrom } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'harry-potter-app';
+
+  personagens: any[] = [];
+
+
+  constructor(private hpService: HpService){
+    hpService.getHp({
+      name: '',
+      house: null,
+      bornDate: null
+    }).subscribe({
+      next: (personagens)=>{
+        this.personagens = personagens;
+        console.log(personagens);
+      },
+      error: ()=>{
+        alert("deu ruim");
+      }
+    });
+    
+  }
 }
